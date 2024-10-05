0cbc5c72600987d6cb2f9bc4c013fcac9ea058f4
yosis2003 <yousif_fadi@hotmail.com>
2024-10-04 19:03:42 -0700

    getrusage!

--- mapreader.c
+++ mapreader.c
@@ -1,26 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #define CHARSBEINGREAD 1000
 
 
-
-
 int main ()
 {
-
     FILE* mapsFileDescriptor = fopen("/proc/self/maps", "r");
-    int maxCharsFgets = CHARSBEINGREAD;
-
     char lineCurrentlyRead[CHARSBEINGREAD];
+    struct rusage performanceStats;
 
 
-    while (fgets(lineCurrentlyRead, maxCharsFgets, mapsFileDescriptor))
+    while (fgets(lineCurrentlyRead, CHARSBEINGREAD, mapsFileDescriptor))
     {
         printf("%s\n", lineCurrentlyRead);
-
     }
 
 
+    getrusage(RUSAGE_SELF, &performanceStats);
+
+    printf("%ld\n", performanceStats.ru_minflt);
+    printf("%ld\n", performanceStats.ru_maxrss);
+    printf("%ld\n", performanceStats.ru_majflt);
+    printf("%ld\n", performanceStats.ru_inblock);
+    printf("%ld\n", performanceStats.ru_oublock);
+    printf("%ld\n", performanceStats.ru_minflt);
+    printf("%ld\n", performanceStats.ru_utime.tv_usec);
+    printf("%ld\n", performanceStats.ru_stime.tv_usec);
 
 }
