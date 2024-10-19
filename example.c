00520fd4f99981a15bc2a647b26b0b4f5946b89f
yosis2003 <yousif_fadi@hotmail.com>
2024-10-19 15:26:48 -0700

    stupid example is not working

---
+++ example.c
@@ -0,0 +1,62 @@
+#include <linux/perf_event.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#define ARRAY_SIZE 1000000
+
+static long
+perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                int cpu, int group_fd, unsigned long flags)
+{
+    int ret;
+
+    ret = syscall(SYS_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+    return ret;
+}
+void cacheMisses() {
+    int *array = malloc(ARRAY_SIZE * sizeof(int));
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        array[rand() % ARRAY_SIZE] = i;
+    }
+    free(array);
+}
+int
+main(void)
+{
+    int                     fd;
+    long long               count;
+    struct perf_event_attr  pe;
+
+    memset(&pe, 0, sizeof(pe));
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(pe);
+    pe.config = PERF_COUNT_HW_CACHE_MISSES;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    pe.exclude_hv = 1;
+
+    fd = perf_event_open(&pe, 0, -1, -1, 0);
+    if (fd == -1) {
+        fprintf(stderr, "Error opening leader %llx\n", pe.config);
+        exit(EXIT_FAILURE);
+    }
+
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+
+    printf("Measuring miss count for this printf\n");
+    cacheMisses();
+
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    read(fd, &count, sizeof(count));
+
+    printf("Captured %lld misses\n", count);
+
+    close(fd);
+}
